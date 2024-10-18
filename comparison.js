168b1e43ed4cef1dc4067830fab929aa62bf4249
Tarannum-Raisha <raishatawsif@gmail.com>
2024-10-18 11:12:12 +0600

    added comparison file

---
+++ comparison.js
@@ -0,0 +1,57 @@
+// console.log(5>10);
+// console.log(5<10);
+// console.log(5 == 10);
+
+// console.log(10 == 10);
+
+//  console.log(10 == 10.1);
+
+// console.log(10 >= 10);
+
+// console.log(15 >= 10);
+
+// console.log(100 <= 10);
+
+
+
+// _______________________________________
+ var peyaraPrice = 40;
+
+ var applePrice = 250 ;
+
+//  console.log(peyaraPrice > applePrice);
+
+// console.log(peyaraPrice <  applePrice);
+
+// console.log(peyaraPrice == applePrice);
+
+// console.log(peyaraPrice >= applePrice);
+ 
+// console.log(peyaraPrice <= applePrice);
+
+
+
+
+// ************
+//  1.  ==
+// 2. === 
+// ************
+
+
+// console.log(10 == '10');
+
+// console.log(10 === '10');
+
+
+// ****************
+//console.log( 10 == 'tomato');
+
+// ****************
+console.log( 10 != 12 );
+console.log( 10 != 10 );
+console.log( 10 !='10' );
+console.log( 10 !== '10');
+
+
+
+
