95bad5bc7a123a67326a429782c0c96e009d58ba
Tarannum-Raisha <raishatawsif@gmail.com>
2024-10-18 12:01:14 +0600

    added if file

---
+++ if.js
@@ -0,0 +1,51 @@
+/*________________________
+ *** If you buy foot for me 
+ *** I will come 
+
+ _______________________
+ *** If biriany price 300 tk 
+ *** I will buy 
+
+
+
+ +++++++++++++++ STRUCTURE ++++++++++++++++++
+  If ( condition ) {
+ } 
+   
+ -------------------------------------
+  If ( condition) {
+  }
+  */
+
+
+if (13 < 10 ){
+    console.log('I have a smaller value');
+}
+
+var birianyPrice = 100;
+var applePrice = 200;
+//  if(birianyPrice>applePrice){
+//     console.log('I will buy apple')
+//  }
+ if(birianyPrice < 300){
+    console.log("mama give me some biriany")
+ }
+
+
+ /* 
+ if ( conditiom){
+   excute code if the condition above is true 
+ }
+else{
+  excute code if the condition is false 
+} 
+
+ */
+
+var weight = 10;
+ if(weight < 20 ){
+    console.log('I will carry it by hands')
+ } 
+ else {
+    console.log('I will rent a rickshaw')
+ }
