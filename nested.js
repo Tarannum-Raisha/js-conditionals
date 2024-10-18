7ab8641edeeb49c4cfcad5f95f1384acaabab7df
Tarannum-Raisha <raishatawsif@gmail.com>
2024-10-18 12:54:36 +0600

    added nested file

---
+++ nested.js
@@ -0,0 +1,22 @@
+const money = 5000;
+if(money > 500){
+    console.log('bro you are borolokxxxx');
+}
+else{
+    if(money > 300 ){
+        console.log('tk to valoi ache ')
+    }
+    else {
+        if(money > 100){
+            console.log('tmi gorib o na borolok o na ')
+        }
+        else{
+            if(money > 10 ){
+                console.log('tumi amar motoi gorib')
+            }
+            else{
+                console.log('tui to fokir purai')
+            }
+        }
+    }
+}
