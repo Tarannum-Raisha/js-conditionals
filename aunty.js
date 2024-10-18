37e8079c50ad3bf19259c3c656c51d02a61bedc4
Tarannum-Raisha <raishatawsif@gmail.com>
2024-10-18 10:53:41 +0600

    added aunty

---
+++ aunty.js
@@ -0,0 +1,15 @@
+// * COMPARISON 
+// 1. BIGGER  >
+// 2. smaller  <
+// 3. equal = 
+
+// **************************************
+// 4. greater then or equal >=
+//5 . less then or equal <= 
+// 6. not equal != 
+
+
+// ********************
+
+// 7. and :&&
+// 8. or :||
---
+++ variable.js
@@ -0,0 +1,25 @@
+// variable =container
+
+
+// number 
+var glassPrice = 20 ;
+console.log(glassPrice);
+
+// string
+
+var address = 'cumilla';
+console.log(address);
+
+
+// boolean
+
+var pass = true ;
+console.log(pass);
+
+
+// operation 
+// 1. + ,- ,* ,% , / 
+//  2. ++ ,-= ,/= ,*= ,*= 
+// 3. isNaN ?
+// 4. null
+// 5. number 
