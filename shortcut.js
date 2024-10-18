2bbd12fa79002b09c34d55acd6d342b03b7ac34c
Tarannum-Raisha <raishatawsif@gmail.com>
2024-10-18 13:49:09 +0600

    added shortcut file

---
+++ shortcut.js
@@ -0,0 +1,21 @@
+// const isLeader = false;
+// if (isLeader){
+//     console.log(
+//         'leader asche! chair charo '
+//     )
+// }
+// else{
+//     console.log(
+//         'vitore dhukte dibi na '
+//     )
+// }
+
+const isPssed = false;
+// age ! dile false bujhay
+// jemon if(isPssed ===false)
+if(!isPssed){
+    console.log('toke biye dibo ')
+}
+else{
+    console.log('tumi amar noyon er moni ')
+}
